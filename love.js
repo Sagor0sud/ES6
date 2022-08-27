dc1b88be393ad2d625300fe006939d4e5b1679af
Sagor0sud <mehedihasan74364@gmail.com>
2022-08-28 02:11:42 +0600

    newJs

---
+++ love.js
@@ -0,0 +1,7 @@
+const numbers = [10, 14, 15, 16, 18];
+const half = numbers.map(n => n / 2);
+const thirds = numbers.map(x => x / 3);
+const fourth = numbers.map(y => y / 4);
+console.log(half);
+console.log(thirds);
+console.log(fourth);
