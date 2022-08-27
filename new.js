275178297e3f05b726b248c4ffb647cbf6fdf6bb
Sagor0sud <mehedihasan74364@gmail.com>
2022-08-28 02:19:34 +0600

    numbers

---
+++ new.js
@@ -0,0 +1,5 @@
+const numbers = [20, 25, 30, 40, 50];
+const newnumbers = numbers.map(x => x * x);
+const mulitipigation = numbers.map(x => x * 4);
+console.log(newnumbers);
+console.log(mulitipigation);
